7f6b5b2dffa78d29998cf1506151d5e4e6ae37b5
Eben-Success <aebenezer237@gmail.com>
2022-12-26 19:50:52 +0000

    update

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/task')
+
+// middleware
+
+app.use(express.json())
+
+// app.get('/api/v1/tasks')  - get all the task
+// app.post('/api/v1/tasks')   - create a new task
+// app.get('/api/v1/tasks/:id') - get a single task
+// app.patch('/api/v1/tasks/:id')  - update task
+// app.delete('/api/v1/tasks/:id')     - delete task
 
 
 // routes
@@ -7,6 +18,7 @@ app.get('/hello', (req, res)=>{
     res.send("Task Manager App")
 })
 
+app.use('/api/v1/tasks', tasks)
 
 
 const port = 3000

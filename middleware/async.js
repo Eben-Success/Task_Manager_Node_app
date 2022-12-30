95feddd8863ce1ba6957ca2742fba8d388380064
Eben-Success <aebenezer237@gmail.com>
2022-12-30 17:12:48 +0000

    update

--- middleware/async.js
+++ middleware/async.js
@@ -1,12 +1,11 @@
-const asyncWrapper = (fn) =>{
+const asyncWrapper = (fn) => {
     return async (req, res, next) => {
         try {
             await fn(req, res, next)
-        } catch (error){
+        } catch (error) {
             next(error)
         }
     }
 }
 
-
-module.exports = asyncWrapper()
+module.exports = asyncWrapper

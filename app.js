d2e8f8c217e3ee4c3ee6a68e6a52fb104ba49491
Eben-Success <aebenezer237@gmail.com>
2022-12-30 17:12:38 +0000

    update

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const connectDB = require('./db/connect')
-const express = require('express')
-const app = express()
-const tasks = require('./routes/task')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/task');
+const connectDB = require('./db/connect');
 require('dotenv').config();
-const notFound = require('./middleware/not-found')
-
-
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
-app.use(express.static('./public'))
-app.use(express.json())
-app.use('/api/v1/tasks', tasks);
 
-app.use(notFound)
-
-// app.get('/api/v1/tasks')  - get all the task
-// app.post('/api/v1/tasks')   - create a new task
-// app.get('/api/v1/tasks/:id') - get a single task
-// app.patch('/api/v1/tasks/:id')  - update task
-// app.delete('/api/v1/tasks/:id')     - delete task
-
+app.use(express.static('./public'));
+app.use(express.json());
 
 // routes
-app.get('/hello', (req, res)=>{
-    res.send("Task Manager App")
-})
 
-connectDB();
+app.use('/api/v1/tasks', tasks);
 
-app.listen(3000, ()=>{
-    console.log("Sever is listening on port 3000")
-})
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();

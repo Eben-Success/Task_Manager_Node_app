ea6afe09038bb15d1ebb8b27e2d8ddf611639ea7
Eben-Success <aebenezer237@gmail.com>
2022-12-30 11:48:28 +0000

    update

--- controller/tasks.js
+++ controller/tasks.js
@@ -35,7 +35,17 @@ const updateTask = (req, res)=>{
     res.send('update task')
 }
 
-const deleteTask = (req, res) =>{
+const deleteTask = async (req, res) =>{
+    try{
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndDelete({_id:taskID})
+
+        if (!task){
+            return res.status(404).json({msg: `No task with id: ${taskID}`})
+        }
+    } catch(error){
+        res.status(500).json({msg:error})
+    }
     res.send('delete task')
 }
 
